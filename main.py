438a45d4d578c1229e1efc17833ac8e464c319ec
Alexey Osipov <sleepshooter@gmail.com>
2021-03-01 19:09:25 +0300

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(0, 100)
+
+while True:
+
+    answer = input("Введите число: ")
+    if not answer or answer == "exit":
+        break
+
+    if not answer.isdigit():
+        print("Введить корректное число!")
+        continue
+
+    user_answer = int(answer)
+
+    if user_answer > number:
+        print("Загаданное число меньше")
+    elif user_answer < number:
+        print("Загаданное число больше")
+    else:
+        print("Вы угадали число!")
+        break
